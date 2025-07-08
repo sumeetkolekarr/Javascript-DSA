770a609a43b84898511a31ef3cfcfd345e46eca6
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-08 16:47:00 +0530

    Loops Started

---
+++ Loops/index.js
@@ -0,0 +1,30 @@
+// for(let i=1;i<22;i++){
+//     console.log(i);
+// }
+
+// Reverse Loop
+// for(let i=1;i<22;i++){
+//     console.log(22-i);
+// }
+
+// An Edge Case for JS // Can be Resolved by using let keyword instead of var keyword 
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// Q. Sum of n natural Number 
+// let num = 5
+// sum = 0
+// for (let i = 0; i <= num; i++) {
+//     sum+=i
+// }
+// console.log(sum);
+
+// Q. Factorial of n natural number
+let num = 5
+fact = 1
+for (let i = 1; i <= num; i++) {
+    fact*=i
+}
+console.log(fact);
