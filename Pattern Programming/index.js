d667500f2905bd4e055d622c7f5c821fa6348390
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-11 15:58:58 +0530

    Pattern Programming Initiated

---
+++ Pattern Programming/index.js
@@ -0,0 +1,50 @@
+// Works in both console and Node.js
+// console.log("Hello");
+// console.log("World");
+
+// Works in Node.js Environment
+// process.stdout.write("Hello ");
+// process.stdout.write("World");
+
+// Taking Input from the user
+let prompt = require("prompt-sync")();
+// let a = prompt("Enter a Number: ");
+// process.stdout.write(a);
+
+// Printing Stars in equal rows and columns
+// let n = Number(prompt("Enter a Number: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// Printing Stars in a triangular shape
+// let n = Number(prompt("Enter a Number: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// Printing Numbers in a triangular shape
+// let n = Number(prompt("Enter a Number: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(j + " ");
+//   }
+//   console.log();
+// }
+
+// Printing Alphabets in a triangular shape
+let n = Number(prompt("Enter a Number: "));
+for (let i = 1; i <= n; i++) {
+  let ascii = 65;
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(String.fromCharCode(ascii) + " ");
+    ascii++;
+  }
+  console.log();
+}
