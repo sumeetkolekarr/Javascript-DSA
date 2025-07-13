ffdb00fdec46909826903ed5bc515c4e90db3026
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-13 16:36:43 +0530

    Arrays Initiated

---
+++ Array/index.js
@@ -0,0 +1,60 @@
+// Array is a linear data structure which stores multiple value in a continuous manner
+let prompt = require("prompt-sync")();
+// let arr = [];
+// arr.push(100);
+// arr.push("sk");
+// arr.push(100);
+// arr.push(false);
+
+// console.log(arr[0]); // prints 100
+// console.log(arr[1]); // prints sk
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr[6] = 20
+// console.log(arr);
+
+// let arr = new Array(3); // Is not a fixed array
+// arr[0] = 12
+// arr[1] = 13
+// arr[2] = 14
+// arr[3] = 15
+// arr[8] = 18
+
+// arr.push(12);
+// console.log(arr);
+
+// Entering an element in array with prompt
+// let arr = new Array(5);
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = Number(prompt("Enter a number to add: "));
+// }
+// console.log(arr);
+
+// Traversing
+let arr = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum);
+
+// Printing largest max number
+// let max =arr[0]
+// for (let i = 1; i < arr.length; i++) {
+//     if(max<arr[i]){
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+// Printing shortest min number
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (max > arr[i]) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
